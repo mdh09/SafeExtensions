3dc59ac4861510d67b77933801662314a526f6c6
mdh09 <md.harvey1985@gmail.com>
2021-03-20 20:02:25 -0500

    Initial check-in of module SafeExtensions

---
+++ SafeExtensions/Strings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace SafeExtensions
+{
+    public static class Strings
+    {
+
+        public static string SafeTrim(this string pStringToTrim, string pDefaultValue = "")
+        {
+            var returnVal = pDefaultValue;
+            if(returnVal == null)
+            {
+                returnVal = "";
+            }
+
+            if(string.IsNullOrWhiteSpace(pStringToTrim))
+            {
+                return returnVal;
+            }
+
+            return pStringToTrim.Trim();
+
+        }
+
+    }
+}
